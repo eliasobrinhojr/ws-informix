848b9c27995af48a9682b3a821333202e671e249
elias jr <elias.dvlpr@gmail.com>
2019-02-11 10:15:51 -0400

    add allow origin

--- src/br/com/componel/ssp/dto/ListaEmpresas.java
+++ src/br/com/componel/ssp/dto/ListaEmpresas.java
@@ -9,19 +9,38 @@ import javax.xml.bind.annotation.XmlRootElement;
 import br.com.componel.ssp.model.Empresa;
 
 @XmlRootElement(name = "empresa")
-
 public class ListaEmpresas implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private List<Empresa> empresas;
+	private int recordsTotal;
+	private int recordsFiltered;
 
-	@XmlElement(name = "empresas")
+	@XmlElement(name = "data")
 	public List<Empresa> getEmpresas() {
 		return empresas;
 	}
 
 	public void setEmpresas(List<Empresa> empresas) {
 		this.empresas = empresas;
+	}
+	
+	
+	@XmlElement(name = "recordsTotal")
+	public int getRecordsTotal() {
+		return recordsTotal;
+	}
+	
+	public void setRecordsTotal(int recordsTotal) {
+		this.recordsTotal = recordsTotal;
+	}
+	@XmlElement(name = "recordsFiltered")
+	public int getRecordsFiltered() {
+		return recordsFiltered;
+	}
+
+	public void setRecordsFiltered(int recordsFiltered) {
+		this.recordsFiltered = recordsFiltered;
 	}
 
 	@Override
--- src/br/com/componel/ssp/servlet/EmpresaServlet.java
+++ src/br/com/componel/ssp/servlet/EmpresaServlet.java
@@ -18,21 +18,30 @@ import br.com.componel.ssp.service.EmpresaService;
 import br.com.componel.ssp.util.ServletUtil;
 
 @WebServlet("/empresas/*")
-public class EmpresaServlet extends HttpServlet  {
+public class EmpresaServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private EmpresaService empService = new EmpresaService();
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		setAccessControlHeaders(resp);
+
 		List<Empresa> empresas = empService.getEmpresas();
 		ListaEmpresas lista = new ListaEmpresas();
 		lista.setEmpresas(empresas);
-		
+		lista.setRecordsFiltered(empresas.size());
+		lista.setRecordsTotal(empresas.size());
+
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		//String json = JAXBUtil.toJson(lista);
+		// String json = JAXBUtil.toJson(lista);
 		String json = gson.toJson(lista);
 		ServletUtil.writeJSON(resp, json);
 	}
-	
+
+	private void setAccessControlHeaders(HttpServletResponse resp) {
+		resp.setHeader("Access-Control-Allow-Origin", "http://dev.grupois.mao");
+		resp.setHeader("Access-Control-Allow-Methods", "GET");
+	}
+
 }
