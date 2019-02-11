70e4ce39df96ff51fe5f9bbc6658ac989b28249b
elias jr <elias.dvlpr@gmail.com>
2019-02-11 15:13:34 -0400

    ajuste resultset

--- src/br/com/componel/ssp/dao/PrestadorDAO.java
+++ src/br/com/componel/ssp/dao/PrestadorDAO.java
@@ -43,30 +43,31 @@ public class PrestadorDAO extends BaseDAO {
 
 	public Prestador createPrestador(ResultSet rs) throws SQLException {
 		Prestador prest = new Prestador();
-		
+				
 		prest.setCdpre(rs.getString(1));
-		prest.setIdpre(rs.getString(2));
-		prest.setCdcgc(rs.getString(3));
-		prest.setCdmun(rs.getString(4));
-		prest.setDspre(rs.getString(5));
-		prest.setEnder(rs.getString(6));
-		prest.setCdcep(rs.getString(7));
-		prest.setCtadi(rs.getString(8));
-		prest.setCtiss(rs.getString(9));
-		prest.setCtirf(rs.getString(10));
-		prest.setCtgps(rs.getString(11));
-		prest.setCtdesc(rs.getString(12));
-		prest.setCtjur(rs.getString(13));
-		prest.setNmpre(rs.getString(14));
-		prest.setStpre(rs.getString(15));
-		prest.setCdemp(rs.getString(16));
-		prest.setCdcent(rs.getString(17));
-		prest.setInscgps(rs.getString(18));
-		prest.setCtpcc(rs.getString(19));
-		prest.setCtcof(rs.getString(20));
-		prest.setCtpis(rs.getString(21));
-		prest.setCtcsl(rs.getString(22));
-		prest.setFlinscprefsp(rs.getString(23));
+		prest.setCdirf(rs.getString(2));
+		prest.setIdpre(rs.getString(3));
+		prest.setCdcgc(rs.getString(4));
+		prest.setCdmun(rs.getString(5));
+		prest.setDspre(rs.getString(6));
+		prest.setEnder(rs.getString(7));
+		prest.setCdcep(rs.getString(8));
+		prest.setCtadi(rs.getString(9));
+		prest.setCtiss(rs.getString(10));
+		prest.setCtirf(rs.getString(11));
+		prest.setCtgps(rs.getString(12));
+		prest.setCtdesc(rs.getString(13));
+		prest.setCtjur(rs.getString(14));
+		prest.setNmpre(rs.getString(15));
+		prest.setStpre(rs.getString(16));
+		prest.setCdemp(rs.getString(17));
+		prest.setCdcent(rs.getString(18));
+		prest.setInscgps(rs.getString(19));
+		prest.setCtpcc(rs.getString(20));
+		prest.setCtcof(rs.getString(21));
+		prest.setCtpis(rs.getString(22));
+		prest.setCtcsl(rs.getString(23));
+		prest.setFlinscprefsp(rs.getString(24));
 		
 		return prest;
 	}
